ac1b1671bead1c95863faab5f2587c4568e8f960
the1994 <makalu171@gmail.com>
2016-09-26 16:29:27 +0900

    변수 사용하기

---
+++ phpjs/3.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <script type="text/javascript">
+      document.write("Hello world")
+    </script>
+    <?php
+    ?>  
+  </body>
+</html>
