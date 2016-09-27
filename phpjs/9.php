414e75a7348052adb814ab26e5bfa8986d68d232
the1994 <makalu171@gmail.com>
2016-09-27 11:39:13 +0900

    [php&javascript] 반복문

---
+++ phpjs/9.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1>JavaScript</h1>
+    <ul>
+    <script type="text/javascript">
+      i = 0;
+      while(i < 10){
+        document.write("<li>hello world</li>");
+        i = i + 1;
+      }
+    </script>
+    </ul>
+    <ul>
+    <h2>php</h2>
+    <?php
+      $i = 0;
+      while($i < 10) {
+        echo "<li>hello world<br /></li>";
+        $i += 1;
+      }
+     ?>
+   </ul>
+  </body>
+</html>
